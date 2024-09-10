87ed00d8b629b91e72537eab0e8960708edc76d0
Jovanni <jovannis8@nycstudents.net>
2024-09-10 17:20:15 -0400

    Remove old code

--- Sanchez_Jovanni_Lab_1.cpp
+++ Sanchez_Jovanni_Lab_1.cpp
@@ -8,54 +8,27 @@ using namespace std;
 
 int main()
 {
-    // This is the code for the first block
-    // The use of \n at the end of every string keeps the code nice and compact.
-    // There is no need for exessive "count << endl;" statements
-    
-    cout << "##############################################################################\n" << endl;
-    cout << "Ways to access the Task Manager on your Windows computer:\n" << endl;
-    cout << "   Press the key combination Ctrl + Shift + Escape\n" << endl;
-    cout << "   Press the key combination Ctrl + Alt + Delete and select “Task Manager”\n" << endl;
-    cout << "   Type “Task Manager” in the Windows Start menu search\n" << endl;
-    cout << "##############################################################################" << endl;
-
     // This is the code for the second block 
     // (Extra credit if the output is surrounded in a Box)
     // NO \n or it BREAKS EVERYTHING >:(
     // \n breaks everything T-T
 
-    // cout << "##############################################################################" << endl;
-    // cout << "#                                                                            #" << endl;
-    // cout << "# Ways to access the Task Manager on your Windows computer:                  #" << endl;
-    // cout << "#                                                                            #" << endl;
-    // cout << "#   Press the key combination Ctrl + Shift + Escape                          #" << endl;
-    // cout << "#                                                                            #" << endl;
-    // cout << "#   Press the key combination Ctrl + Alt + Delete and select “Task Manager”  #" << endl;
-    // cout << "#                                                                            #" << endl;
-    // cout << "#   Type “Task Manager” in the Windows Start menu search                     #" << endl;
-    // cout << "#                                                                            #" << endl;
-    // cout << "##############################################################################" << endl;
+    cout << "##############################################################################" << endl;
+    cout << "#                                                                            #" << endl;
+    cout << "# Ways to access the Task Manager on your Windows computer:                  #" << endl;
+    cout << "#                                                                            #" << endl;
+    cout << "#   Press the key combination Ctrl + Shift + Escape                          #" << endl;
+    cout << "#                                                                            #" << endl;
+    cout << "#   Press the key combination Ctrl + Alt + Delete and select “Task Manager”  #" << endl;
+    cout << "#                                                                            #" << endl;
+    cout << "#   Type “Task Manager” in the Windows Start menu search                     #" << endl;
+    cout << "#                                                                            #" << endl;
+    cout << "##############################################################################" << endl;
 
     return 0;
 }
 
-// OUTPUTS BELOW
-
-// [Running] cd "c:\Users\College\Documents\GitHub\lab-1-cout-statements-Jovanni-Sanchez\" && g++ Sanchez_Jovanni_Lab_1.cpp -o Sanchez_Jovanni_Lab_1 && "c:\Users\College\Documents\GitHub\lab-1-cout-statements-Jovanni-Sanchez\"Sanchez_Jovanni_Lab_1
-// ############################################################################
-
-// Ways to access the Task Manager on your Windows computer:
-
-//    Press the key combination Ctrl + Shift + Escape
-
-//    Press the key combination Ctrl + Alt + Delete and select “Task Manager”
- 
-//    Type “Task Manager” in the Windows Start menu search
-
-// ###########################################################################
-
-// [Done] exited with code=0 in 1.504 seconds
-
+// OUTPUT BELOW
 
 // [Running] cd "c:\Users\College\Documents\GitHub\lab-1-cout-statements-Jovanni-Sanchez\" && g++ Sanchez_Jovanni_Lab_1.cpp -o Sanchez_Jovanni_Lab_1 && "c:\Users\College\Documents\GitHub\lab-1-cout-statements-Jovanni-Sanchez\"Sanchez_Jovanni_Lab_1
 // ##############################################################################
