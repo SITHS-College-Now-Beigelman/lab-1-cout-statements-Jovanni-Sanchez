3d948b46cb513c40ae88feb5d8b164ad11fd35fd
Jovanni <jovannis8@nycstudents.net>
2024-09-10 16:56:44 -0400

    Source Code
    The Source Code on Github

---
+++ Sanchez_Jovanni_Lab_1.cpp
@@ -0,0 +1,57 @@
+// Jovanni Sanchez
+// September 10 2024
+// Lab 1
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // This is the code for the first code
+    // The use of \n at the end of every string keeps the code nice and compact.
+    // There is no need for exessive "count << endl;" statements
+    cout << "##############################################################################\n" << endl;
+    cout << "Ways to access the Task Manager on your Windows computer:\n" << endl;
+    cout << "   Press the key combination Ctrl + Shift + Escape\n" << endl;
+    cout << "   Press the key combination Ctrl + Alt + Delete and select “Task Manager”\n" << endl;
+    cout << "   Type “Task Manager” in the Windows Start menu search\n" << endl;
+    cout << "#############################################################################" << endl;
+
+    // This is the code for the second code (BOX)
+    // NO \n >:(
+    // cout << "##############################################################################" << endl;
+    // cout << "# Ways to access the Task Manager on your Windows computer:                  #" << endl;
+    // cout << "#   Press the key combination Ctrl + Shift + Escape                          #" << endl;
+    // cout << "#   Press the key combination Ctrl + Alt + Delete and select “Task Manager”  #" << endl;
+    // cout << "#   Type “Task Manager” in the Windows Start menu search                     #" << endl;
+    // cout << "##############################################################################" << endl;
+
+    return 0;
+}
+
+// [Running] cd "c:\Users\College\Documents\GitHub\lab-1-cout-statements-Jovanni-Sanchez\" && g++ Sanchez_Jovanni_Lab_1.cpp -o Sanchez_Jovanni_Lab_1 && "c:\Users\College\Documents\GitHub\lab-1-cout-statements-Jovanni-Sanchez\"Sanchez_Jovanni_Lab_1
+// ############################################################################
+
+// Ways to access the Task Manager on your Windows computer:
+
+//    Press the key combination Ctrl + Shift + Escape
+
+//    Press the key combination Ctrl + Alt + Delete and select “Task Manager”
+ 
+//    Type “Task Manager” in the Windows Start menu search
+
+// ###########################################################################
+
+// [Done] exited with code=0 in 1.504 seconds
+
+
+// [Running] cd "c:\Users\College\Documents\GitHub\lab-1-cout-statements-Jovanni-Sanchez\" && g++ Sanchez_Jovanni_Lab_1.cpp -o Sanchez_Jovanni_Lab_1 && "c:\Users\College\Documents\GitHub\lab-1-cout-statements-Jovanni-Sanchez\"Sanchez_Jovanni_Lab_1
+// ##############################################################################
+// # Ways to access the Task Manager on your Windows computer:                  #
+// #   Press the key combination Ctrl + Shift + Escape                          #
+// #   Press the key combination Ctrl + Alt + Delete and select “Task Manager”  #
+// #   Type “Task Manager” in the Windows Start menu search                     #
+// ##############################################################################
+
+// [Done] exited with code=0 in 1.605 seconds
