950051b28b41b29477501abe538e48a74b6d3ac7
Jovanni <jovannis8@nycstudents.net>
2024-09-10 17:06:26 -0400

    Improved the box
    The output is now more spaced out

--- Sanchez_Jovanni_Lab_1.cpp
+++ Sanchez_Jovanni_Lab_1.cpp
@@ -17,16 +17,21 @@ int main()
     cout << "   Press the key combination Ctrl + Shift + Escape\n" << endl;
     cout << "   Press the key combination Ctrl + Alt + Delete and select “Task Manager”\n" << endl;
     cout << "   Type “Task Manager” in the Windows Start menu search\n" << endl;
-    cout << "#############################################################################" << endl;
+    cout << "##############################################################################" << endl;
 
     // This is the code for the second code (BOX)
     // NO \n >:(
 
     // cout << "##############################################################################" << endl;
+    // cout << "#                                                                            #" << endl;
     // cout << "# Ways to access the Task Manager on your Windows computer:                  #" << endl;
+    // cout << "#                                                                            #" << endl;
     // cout << "#   Press the key combination Ctrl + Shift + Escape                          #" << endl;
+    // cout << "#                                                                            #" << endl;
     // cout << "#   Press the key combination Ctrl + Alt + Delete and select “Task Manager”  #" << endl;
+    // cout << "#                                                                            #" << endl;
     // cout << "#   Type “Task Manager” in the Windows Start menu search                     #" << endl;
+    // cout << "#                                                                            #" << endl;
     // cout << "##############################################################################" << endl;
 
     return 0;
@@ -52,10 +57,15 @@ int main()
 
 // [Running] cd "c:\Users\College\Documents\GitHub\lab-1-cout-statements-Jovanni-Sanchez\" && g++ Sanchez_Jovanni_Lab_1.cpp -o Sanchez_Jovanni_Lab_1 && "c:\Users\College\Documents\GitHub\lab-1-cout-statements-Jovanni-Sanchez\"Sanchez_Jovanni_Lab_1
 // ##############################################################################
+// #                                                                            #
 // # Ways to access the Task Manager on your Windows computer:                  #
+// #                                                                            #
 // #   Press the key combination Ctrl + Shift + Escape                          #
+// #                                                                            #
 // #   Press the key combination Ctrl + Alt + Delete and select “Task Manager”  #
+// #                                                                            #
 // #   Type “Task Manager” in the Windows Start menu search                     #
+// #                                                                            #
 // ##############################################################################
 
-// [Done] exited with code=0 in 1.605 seconds
+// [Done] exited with code=0 in 1.604 seconds
